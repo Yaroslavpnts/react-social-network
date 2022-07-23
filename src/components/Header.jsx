c354babe7c59918347fc54e4b677443983388dfd
Yaroslavpnts <rofem2335@gmail.com>
2022-07-23 16:15:45 +0300

    after lesson 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://s1.mzstatic.com/us/r30/Purple4/v4/28/43/01/284301c1-c09b-0e4d-8f13-81b2554b8d8a/mzl.noihcpxs.png"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://thumbs.dreamstime.com/b/cosmos-beauty-deep-space-elements-image-furnished-nasa-science-fiction-art-102581846.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDkvFCLSMbUU6Bqb1m-0y3LPAQ7_Gcs-PNZw&usqp=CAU"
+          alt=""
+        />
+      </div>
+      <div>
+        my posts
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
