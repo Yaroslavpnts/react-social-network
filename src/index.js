b954e74da7109622493a7adf8ac12268fcb514b3
Yaroslavpnts <rofem2335@gmail.com>
2022-07-27 21:45:13 +0300

    after lesson 37

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe } from './redux/state.js';
+import store from './redux/state.js';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { addPost, updateNewPostText, addMessage, updateNewMessageText } from './redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -14,19 +13,19 @@ let rerenderEntireTree = state => {
       <BrowserRouter>
         <App
           state={state}
-          addPost={addPost}
-          updateNewPostText={updateNewPostText}
-          addMessage={addMessage}
-          updateNewMessageText={updateNewMessageText}
+          addPost={store.addPost.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)}
+          addMessage={store.addMessage.bind(store)}
+          updateNewMessageText={store.updateNewMessageText.bind(store)}
         />
       </BrowserRouter>
     </React.StrictMode>
   );
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,98 +1,107 @@
-let rerenderEntireTree;
+// let rerenderEntireTree;
 
-let state = {
-  profilePage: {
-    postsData: [
-      { id: 1, message: 'Hi, how are you?', likesCount: 12 },
-      { id: 2, message: "It's my first post", likesCount: 11 },
-      { id: 3, message: 'Bla-bla', likesCount: 11 },
-      { id: 4, message: 'Hello everybody', likesCount: 11 },
-    ],
-    newPostText: '',
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+        { id: 2, message: "It's my first post", likesCount: 11 },
+        { id: 3, message: 'Bla-bla', likesCount: 11 },
+        { id: 4, message: 'Hello everybody', likesCount: 11 },
+      ],
+      newPostText: '',
+    },
+    dialogsPage: {
+      messagesData: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How are you?' },
+        { id: 3, message: 'Yo' },
+        { id: 4, message: 'Yo' },
+        { id: 5, message: 'Yo' },
+      ],
+      dialogsData: [
+        { id: 1, name: 'Dimych' },
+        { id: 2, name: 'Yaroslav' },
+        { id: 3, name: 'Dasha' },
+        { id: 4, name: 'Viktor' },
+        { id: 5, name: 'Alexander' },
+        { id: 6, name: 'Mariya' },
+      ],
+      newMessageText: '',
+    },
+    sidebar: {
+      friends: [
+        {
+          id: 1,
+          name: 'Mariya',
+          photo:
+            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+        },
+        {
+          id: 2,
+          name: 'Dimych',
+          photo:
+            'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+        },
+        {
+          id: 3,
+          name: 'Alexander',
+          photo:
+            'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+        },
+      ],
+    },
   },
-  dialogsPage: {
-    messagesData: [
-      { id: 1, message: 'Hi' },
-      { id: 2, message: 'How are you?' },
-      { id: 3, message: 'Yo' },
-      { id: 4, message: 'Yo' },
-      { id: 5, message: 'Yo' },
-    ],
-    dialogsData: [
-      { id: 1, name: 'Dimych' },
-      { id: 2, name: 'Yaroslav' },
-      { id: 3, name: 'Dasha' },
-      { id: 4, name: 'Viktor' },
-      { id: 5, name: 'Alexander' },
-      { id: 6, name: 'Mariya' },
-    ],
-    newMessageText: '',
+
+  getState() {
+    return this._state;
   },
-  sidebar: {
-    friends: [
-      {
-        id: 1,
-        name: 'Mariya',
-        photo:
-          'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
-      },
-      {
-        id: 2,
-        name: 'Dimych',
-        photo:
-          'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
-      },
-      {
-        id: 3,
-        name: 'Alexander',
-        photo:
-          'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
-      },
-    ],
+
+  _callSubscriber() {},
+
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+
+    this._state.profilePage.postsData.push(newPost);
+
+    this._state.profilePage.newPostText = '';
+
+    this._callSubscriber(this._state);
+  },
+
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+
+  updateNewMessageText(newText) {
+    this._state.dialogsPage.newMessageText = newText;
+    this._callSubscriber(this._state);
+  },
+
+  addMessage() {
+    let newMessage = {
+      id: this._state.dialogsPage.messagesData.length + 1,
+      message: this._state.dialogsPage.newMessageText,
+    };
+
+    this._state.dialogsPage.messagesData.push(newMessage);
+
+    this._state.dialogsPage.newMessageText = '';
+
+    this._callSubscriber(this._state);
+  },
+
+  subscribe(observer) {
+    this._callSubscriber = observer; // паттерн observer
   },
 };
 
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
+export default store;
 
-  state.profilePage.postsData.push(newPost);
-
-  state.profilePage.newPostText = '';
-
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = newText => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const updateNewMessageText = newText => {
-  state.dialogsPage.newMessageText = newText;
-  rerenderEntireTree(state);
-};
-
-export const addMessage = () => {
-  let newMessage = {
-    id: state.dialogsPage.messagesData.length + 1,
-    message: state.dialogsPage.newMessageText,
-  };
-
-  state.dialogsPage.messagesData.push(newMessage);
-
-  state.dialogsPage.newMessageText = '';
-
-  rerenderEntireTree(state);
-};
-
-export const subscribe = observer => {
-  rerenderEntireTree = observer; // паттерн observer
-};
-
-export default state;
-
-// Будем внедрять объект store (OOP)
+window.store = store;
+// export default state;
